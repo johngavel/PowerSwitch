51655ee0158aec5a724d39695b7cf82d0fdc3204
John J. Gavel <john.gavel@gmail.com>
2025-11-03 10:59:29 -0500

    Auto-commit on 2025-11-03 10:59:29

--- build.sh
+++ build.sh
@@ -12,18 +12,18 @@ CURRENT_DIR="$2"
 case "$BUILD" in
   --clean)
     Delete "$CURRENT_DIR"/favicon.h
-    create_libraries_used.sh --clean $HOME_DIR/LibraryV1.0/GavelGPIO
-    create_libraries_used.sh --clean $HOME_DIR/LibraryV1.0/GavelLicense
+    create_libraries_used.sh --clean "$LIBRARY_GAVEL/$LIBRARYV1"/GavelGPIO
+    create_libraries_used.sh --clean "$LIBRARY_GAVEL/$LIBRARYV1"/GavelLicense
     ;;
   --pre)
     createfileheader.sh "$CURRENT_DIR"/assets/favicon_blue.ico "$CURRENT_DIR"/favicon.h favicon
-    create_libraries_used.sh --build $HOME_DIR/LibraryV1.0/GavelGPIO TCA9555_GPIO
-    create_libraries_used.sh --build $HOME_DIR/LibraryV1.0/GavelLicense
-    #create_libraries_used.sh --build $HOME_DIR/LibraryV1.0/GavelLicense ETHERNET_USED TERMINAL_USED TCA9555_USED I2C_EEPROM_USED DHT_SENSOR_LIBRARY_USED ADAFRUIT_SSD1306_USED ADAFRUIT_GFX_LIBRARY_USED ADAFRUIT_BUSIO_USED ADAFRUIT_UNIFIED_SENSOR_USED
+    create_libraries_used.sh --build "$LIBRARY_GAVEL/$LIBRARYV1"/GavelGPIO TCA9555_GPIO
+    create_libraries_used.sh --build "$LIBRARY_GAVEL/$LIBRARYV1"/GavelLicense
+    #create_libraries_used.sh --build "$LIBRARY_GAVEL/$LIBRARYV1"/GavelLicense ETHERNET_USED TERMINAL_USED TCA9555_USED I2C_EEPROM_USED DHT_SENSOR_LIBRARY_USED ADAFRUIT_SSD1306_USED ADAFRUIT_GFX_LIBRARY_USED ADAFRUIT_BUSIO_USED ADAFRUIT_UNIFIED_SENSOR_USED
     ;;
   --post)
-    create_libraries_used.sh --clean $HOME_DIR/LibraryV1.0/GavelGPIO
-    create_libraries_used.sh --clean $HOME_DIR/LibraryV1.0/GavelLicense
+    create_libraries_used.sh --clean "$LIBRARY_GAVEL/$LIBRARYV1"/GavelGPIO
+    create_libraries_used.sh --clean "$LIBRARY_GAVEL/$LIBRARYV1"/GavelLicense
     ;;
   --build)
     createfileheader.sh "$CURRENT_DIR"/assets/favicon_blue.ico "$CURRENT_DIR"/favicon.h favicon
