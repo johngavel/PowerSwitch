a163e80b37b65f7bef504d41d2ec533932db1879
John J. Gavel <john.gavel@gmail.com>
2026-01-25 08:30:28 -0500

    Auto-commit on 2026-01-25 08:30:28

--- files/webpage_all.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GAVEL_POWER_WEBPAGE_ALL_H
-#define __GAVEL_POWER_WEBPAGE_ALL_H
-
-#include "favicon_blueico.h"
-#include "favicon_greenico.h"
-#include "indexhtml.h"
-#include "powerjs.h"
-
-#endif // __GAVEL_POWER_WEBPAGE_ALL_H 
